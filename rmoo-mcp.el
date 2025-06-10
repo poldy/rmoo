65740c53460b3bd20d4792087f6f9bfabbf28553
Darren Bane <darren.bane.ac@posteo.net>
2025-06-10 23:46:14 +0000

    Fix play-sound on macOS

--- rmoo-mcp.el
+++ rmoo-mcp.el
@@ -302,6 +302,26 @@
 		   "2.0"
 		   nil)
 
+;; on macos, fix "This Emacs binary lacks sound support" 
+;; - https://github.com/leoliu/play-sound-osx/blob/master/play-sound.el
+;; - update according to https://github.com/leoliu/play-sound-osx/issues/2#issuecomment-1088360638
+(when (eq system-type 'darwin)
+  (unless (and (fboundp 'play-sound-internal)
+               (subrp (symbol-function 'play-sound-internal)))
+    (defun play-sound-internal (sound)
+      "Internal function for `play-sound' (which see)."
+      (or (eq (car-safe sound) 'sound)
+          (signal 'wrong-type-argument (list sound)))
+      (cl-destructuring-bind (&key file data volume device)
+          (cdr sound)
+	
+        (and (or data device)
+             (error "DATA and DEVICE arg not supported"))
+	
+        (apply #'start-process "afplay" nil
+               "afplay" (append (and volume (list "-v" volume))
+                                (list (expand-file-name file data-directory))))))))
+
 (defun rmoo-mcp-do-sound (name v l p type u)
   (let* ((dirname (file-name-directory name))
 	 (basename (file-name-nondirectory name))
